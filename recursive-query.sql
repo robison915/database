5b64601f778bd8c3ae9d66e83d3c4e15cddb09bc
robison915 <37427219+robison915@users.noreply.github.com>
2019-02-21 21:35:49 -0300

    Update recursive-query.sql

--- recursive-query.sql
+++ recursive-query.sql
@@ -1,9 +1,12 @@
--- Consulta recursiva, unindo campos
--- OBS - Alias do UNPIVOT, quando há mais de um campo, deve ser numérico
+-- Recursive query, joining columns
+-- OBS - the UNPIVOT alias, when there is more than one column, must be numeric
+-- DOC https://docs.oracle.com/cd/B19306_01/server.102/b14200/queries003.htm
+-- DOC https://oracle-base.com/articles/11g/pivot-and-unpivot-operators-11gr1
+-- Can be tested on https://livesql.oracle.com
+
+
 SELECT coop, conta, nivel,col, PATH_COOP
   FROM ( 
-
-
   	     SELECT DISTINCT I.COOP_ORIGEM, I.CONTA_ORIGEM, I.COOP_DESTINO, I.CONTA_DESTINO, level nivel, SYS_CONNECT_BY_PATH(COOP_DESTINO||'-'||COOP_ORIGEM, '/') "PATH_COOP"
            FROM (select '0718' COOP_DESTINO, '811473' CONTA_DESTINO, '0101' COOP_ORIGEM, '123456' CONTA_ORIGEM from dual union all
            	     select '0101' COOP_DESTINO, '123456' CONTA_DESTINO, '1006' COOP_ORIGEM, '654321' CONTA_ORIGEM from dual union all
@@ -15,7 +18,6 @@ SELECT coop, conta, nivel,col, PATH_COOP
         NOCYCLE PRIOR I.COOP_ORIGEM = I.COOP_DESTINO 
             AND PRIOR I.CONTA_ORIGEM = I.CONTA_DESTINO 
             AND LEVEL <= 3
-			 
-
-			 ) x UNPIVOT ( (COOP,CONTA) FOR COL IN ( (COOP_ORIGEM, CONTA_ORIGEM) as 1,(COOP_DESTINO, CONTA_DESTINO ) as 2 ))
+	  
+	 ) x UNPIVOT ( (COOP,CONTA) FOR COL IN ( (COOP_ORIGEM, CONTA_ORIGEM) as 1,(COOP_DESTINO, CONTA_DESTINO ) as 2 ))
  order by nivel,col desc;
