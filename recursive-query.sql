abca084feed0c3955866dcb63f9572e405c68954
Robison Souza <robison.nunes01@gmail.com>
2019-02-21 21:14:33 -0300

    recursive query

---
+++ recursive-query.sql
@@ -0,0 +1,21 @@
+-- Consulta recursiva, unindo campos
+-- OBS - Alias do UNPIVOT, quando há mais de um campo, deve ser numérico
+SELECT coop, conta, nivel,col, PATH_COOP
+  FROM ( 
+
+
+  	     SELECT DISTINCT I.COOP_ORIGEM, I.CONTA_ORIGEM, I.COOP_DESTINO, I.CONTA_DESTINO, level nivel, SYS_CONNECT_BY_PATH(COOP_DESTINO||'-'||COOP_ORIGEM, '/') "PATH_COOP"
+           FROM (select '0718' COOP_DESTINO, '811473' CONTA_DESTINO, '0101' COOP_ORIGEM, '123456' CONTA_ORIGEM from dual union all
+           	     select '0101' COOP_DESTINO, '123456' CONTA_DESTINO, '1006' COOP_ORIGEM, '654321' CONTA_ORIGEM from dual union all
+           	     select '0104' COOP_DESTINO, '147852' CONTA_DESTINO, '0605' COOP_ORIGEM, '963258' CONTA_ORIGEM from dual union all
+           	     select '0605' COOP_DESTINO, '963258' CONTA_DESTINO, '0978' COOP_ORIGEM, '365478' CONTA_ORIGEM from dual
+           	     ) I
+          START WITH I.COOP_DESTINO = '0718' and I.CONTA_DESTINO = '811473'
+        CONNECT BY 
+        NOCYCLE PRIOR I.COOP_ORIGEM = I.COOP_DESTINO 
+            AND PRIOR I.CONTA_ORIGEM = I.CONTA_DESTINO 
+            AND LEVEL <= 3
+			 
+
+			 ) x UNPIVOT ( (COOP,CONTA) FOR COL IN ( (COOP_ORIGEM, CONTA_ORIGEM) as 1,(COOP_DESTINO, CONTA_DESTINO ) as 2 ))
+ order by nivel,col desc;
